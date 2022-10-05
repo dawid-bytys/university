24137a699149f6e55c8cc72d8401c08c3209ad46
salvia-dev <salviadev@protonmail.com>
2022-10-05 23:06:50 +0200

    feat: add raport to the num1

--- term-3/numerical-methods/num1/main.py
+++ term-3/numerical-methods/num1/main.py
@@ -1,32 +1,46 @@
-import matplotlib
-import numpy as np
-import matplotlib.pyplot as plt
+import sys
+import warnings
 from typing import Callable
 
-matplotlib.rcParams.update(
-    {
-        "text.usetex": True,
-    }
-)
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+
+warnings.filterwarnings("ignore")
+matplotlib.rcParams.update({"text.usetex": True, "font.size": 8})
+
+axis_font = {"fontname": "Arial", "size": "12", "weight": "black"}
 
 
-def approximate_derivative_a(x: float, h: float, fn: Callable) -> float:
+def discrete_derivative_a(x: float, h: float, fn: Callable[[float], float]) -> float:
     return (fn(x + h) - fn(x)) / h
 
 
-def approximate_derivative_b(x: float, h: float, fn: Callable) -> float:
+def discrete_derivative_b(x: float, h: float, fn: Callable[[float], float]) -> float:
     return (fn(x + h) - fn(x - h)) / (2 * h)
 
 
-def absolute_error(fn_1: Callable, fn_2: Callable, x: float) -> float:
+def absolute_error(
+    fn_1: Callable[[float], float], fn_2: Callable[[float], float], x: float
+) -> float:
     return abs(fn_1(x) - fn_2(x))
 
 
-def draw_plot(x: float, h: float, fn: Callable, D_fn: Callable, filename: str) -> None:
-    error_a = absolute_error(lambda i: approximate_derivative_a(i, h, fn), D_fn, x)
-    error_b = absolute_error(lambda i: approximate_derivative_b(i, h, fn), D_fn, x)
+def draw_plot(
+    x: float,
+    h: float,
+    fn: Callable[[float], float],
+    D_fn: Callable[[float], float],
+    filename: str,
+) -> None:
+    error_a = absolute_error(
+        lambda x_prim: discrete_derivative_a(x_prim, h, fn), D_fn, x
+    )
+    error_b = absolute_error(
+        lambda x_prim: discrete_derivative_b(x_prim, h, fn), D_fn, x
+    )
 
-    fig = plt.figure(dpi=300)
+    fig = plt.figure(figsize=(10, 5), dpi=600)
     axes = fig.gca()
 
     plt.plot(h, error_a, linewidth=1.5, color="red", label="Method (A)")
@@ -35,8 +49,8 @@ def draw_plot(x: float, h: float, fn: Callable, D_fn: Callable, filename: str) -
     axes.set_xscale("log")
     axes.set_yscale("log")
 
-    axes.set_xlabel("Log scale of h", fontsize=12, fontweight="bold")
-    axes.set_ylabel("Log scale of Error (x = 0.2)", fontsize=12, fontweight="bold")
+    axes.set_xlabel("log(h)", **axis_font)
+    axes.set_ylabel("log(E(h)) for x = 0.2", **axis_font)
 
     axes.legend()
     plt.grid()
@@ -44,15 +58,23 @@ def draw_plot(x: float, h: float, fn: Callable, D_fn: Callable, filename: str) -
 
 
 if __name__ == "__main__":
-    x_1 = np.float32(0.2)
-    x_2 = np.float64(0.2)
-    h_1 = np.float32(np.logspace(-14, 1, 1000))
-    h_2 = np.float64(np.logspace(-24, 1, 1000))
+    x_f32 = np.float32(0.2)
+    x_f64 = np.float64(0.2)
+    h_f32 = np.float32(np.logspace(start=-10, stop=1, num=1500))
+    h_f64 = np.float64(np.logspace(start=-18, stop=1, num=1500))
+
+    print("Drawing plots...")
 
     """sin(x)"""
-    draw_plot(x_1, h_1, np.sin, np.cos, "./float32_sin.png")
-    draw_plot(x_2, h_2, np.sin, np.cos, "./float64_sin.png")
+
+    draw_plot(x_f32, h_f32, np.sin, np.cos, "./float32_sin.png")
+    draw_plot(x_f64, h_f64, np.sin, np.cos, "./float64_sin.png")
 
     """exp(x)"""
-    draw_plot(x_1, h_1, np.exp, np.exp, "./float32_exp.png")
-    draw_plot(x_2, h_2, np.exp, np.exp, "./float64_exp.png")
+
+    draw_plot(x_f32, h_f32, np.exp, np.exp, "./float32_exp.png")
+    draw_plot(x_f64, h_f64, np.exp, np.exp, "./float64_exp.png")
+
+    print("Plots have been successfully saved!")
+
+    sys.exit(0)
