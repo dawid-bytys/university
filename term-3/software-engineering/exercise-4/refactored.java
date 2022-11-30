c3853e99264e7008f35a431f85bf0bcbabd0a5b1
salvia-dev <salviadev@protonmail.com>
2022-11-30 13:44:41 +0100

    fix: exercise-4

--- term-3/software-engineering/exercise-4/refactored.java
+++ term-3/software-engineering/exercise-4/refactored.java
@@ -1,17 +1,41 @@
-class Order {
-    boolean isSameString(final String s1, final String s2) {
-        if (s1 == null) {
-            return s2 == null;
-        }
-        return s1.equals(s2);
+abstract class Rental {
+    protected String fee;
+    protected Date dueDate;
+    protected Date rentDate;
+
+    public boolean isOverdue() {
+        return dueDate.before(new Date());
+    }
+
+    public double getTotalFee() {
+        return isOverdue() ? 1.2 * rentalFee : rentalFee;
     }
 }
 
-class Mail {
-    static boolean isSameString(final String s1, final String s2) {
-        if (s1 == null) {
-            return s2 == null;
-        }
-        return s1.equals(s2);
+public class BookRental extends Rental {
+    private String bookTitle;
+    private String author;
+
+    public BookRental(final String bookTitle, final String author, final String fee, final Date dueDate,
+            final Date rentDate) {
+        this.bookTitle = bookTitle;
+        this.author = author;
+        this.fee = fee;
+        this.dueDate = dueDate;
+        this.rentDate = rentDate;
+    }
+}
+
+public class MovieRental extends Rental {
+    private String movieTitle;
+    private String classification;
+
+    public MovieRental(final String movieTitle, final String classification, final String fee, final Date dueDate,
+            final Date rentDate) {
+        this.movieTitle = movieTitle;
+        this.classification = classification;
+        this.fee = fee;
+        this.dueDate = dueDate;
+        this.rentDate = rentDate;
     }
 }
