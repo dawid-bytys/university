97ed8b0801ad5c2d06a52e8a3764fe7c1bed0180
salvia-dev <salviadev@protonmail.com>
2023-06-03 23:31:31 +0200

    cleanup: maze

--- term-4/algorithms-and-data-structures-2/maze-dfs/main.js
+++ term-4/algorithms-and-data-structures-2/maze-dfs/main.js
@@ -4,25 +4,25 @@ const ctx = canvas.getContext("2d");
 let size = 35;
 let cellSize = canvas.width / size;
 
-const generateMaze = () => {
+function generateMaze() {
   size = parseInt(document.getElementById("size").value);
   cellSize = canvas.width / size;
   let maze = createMaze(size);
   drawMaze(maze);
-};
+}
 
-const createEmptyMaze = (size) => {
+function createEmptyMaze(size) {
   const maze = new Array(size)
     .fill(null)
     .map(() => new Array(size).fill(false));
   return maze;
-};
+}
 
-const isValid = (x, y, size) => {
+function isMazeValid(x, y, size) {
   return x >= 0 && x < size && y >= 0 && y < size;
-};
+}
 
-const createMaze = (size) => {
+function createMaze(size) {
   const maze = createEmptyMaze(size);
   const stack = [];
   const directions = [
@@ -46,7 +46,7 @@ const createMaze = (size) => {
     for (const [dx, dy] of directions) {
       const x = current[0] + 2 * dx;
       const y = current[1] + 2 * dy;
-      if (isValid(x, y, size) && !maze[x][y]) {
+      if (isMazeValid(x, y, size) && !maze[x][y]) {
         neighbors.push([x, y, dx, dy]);
       }
     }
@@ -62,9 +62,9 @@ const createMaze = (size) => {
   }
 
   return maze;
-};
+}
 
-const drawMaze = (maze) => {
+function drawMaze(maze) {
   ctx.fillStyle = "#fff";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
@@ -76,7 +76,11 @@ const drawMaze = (maze) => {
       }
     }
   }
-};
+}
 
-generateMaze();
-generateBtn.addEventListener("click", generateMaze);
+function main() {
+  generateMaze();
+  generateBtn.addEventListener("click", generateMaze);
+}
+
+main();
