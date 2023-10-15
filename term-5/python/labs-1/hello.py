c13261fbabbb54a7529d5f710a6141c19f361144
dawid-bytys <dawid.bytys@proton.me>
2023-10-15 20:32:11 +0200

    feat(python): add labs-1

---
+++ term-5/python/labs-1/hello.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding=utf-8
+#
+# hello.py
+#
+# Pierwszy program w Pythonie.
+# W pierwszej linii mamy ścieżkę dostępu do interpretera.
+# W drugiej linii informacja o kodowaniu znaków w pliku.
+# Dzięki temu można w pliku stosować polskie znaki w napisach.
+# Po znaku '#' mamy komentarz do końca linii.
+
+print("Hello, world!")  # działa dla Pythona 2 i 3
