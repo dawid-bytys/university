ec9a697638d456475b02811ae59d857434c64b87
dawid-bytys <dawid.bytys@proton.me>
2023-10-25 22:10:10 +0200

    feat(python): add 3.1 in labs-3

---
+++ term-5/python/labs-3/3.2.py
@@ -0,0 +1,31 @@
+# Co jest złego w kodzie?
+
+# 1. Próba przypisania wartości do zmiennej, która jest typu None.
+"""
+L = [3, 5, 4] ; L = L.sort()
+"""
+
+# 2. Próba przypisania trzech wartości do dwóch zmiennych.
+"""
+x, y = 1, 2, 3
+"""
+
+# 3. Próba zmienienia wartości w krotce. Krotki są niemutowalne.
+"""
+X = 1, 2, 3 ; X[1] = 4
+"""
+
+# 4. Próba odwołania się do indeksu, który nie istnieje.
+"""
+X = [1, 2, 3] ; X[3] = 4
+"""
+
+# 5. Próba odwołania się do metody, która nie istnieje dla danego typu.
+"""
+X = "abc" ; X.append("d")
+"""
+
+# 6. Funkcja pow przyjmuje dwa argumenty.
+"""
+L = list(map(pow, range(8)))
+"""
